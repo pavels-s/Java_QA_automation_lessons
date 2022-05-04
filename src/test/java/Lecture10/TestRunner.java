4ae0a94555a926a56e7dbc1364e692cb0c3aa667
pavels-s <pavels.sarkels@gmail.com>
2022-05-04 09:47:06 +0300

    Uppercase change

--- src/test/java/Lecture10/TestRunner.java
+++ src/test/java/Lecture10/TestRunner.java
@@ -7,7 +7,7 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions (plugin = "pretty",
-        features = {"src\\test\\resources\\Features"},
+        features = {"src\\test\\resources\\features"},
         glue = {"Lecture10\\steps"},
         tags = "@SSlv"
 
